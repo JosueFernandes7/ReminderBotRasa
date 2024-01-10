42b276d5a0e6c59dd3f27a391eccf4c9d2b8c393
Josué Fernandes <josue7mf@hotmail.com>
2024-01-10 09:14:39 -0300

    :sparkles: feat: Add db + action integration

--- actions/actions.py
+++ actions/actions.py
@@ -1,31 +1,8 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-
+from db import MongoDBConnection
+from config import settings
 
 class ActionTarefa(Action):
     def name(self):
@@ -37,12 +14,23 @@ class ActionTarefa(Action):
         nome_tarefa = tracker.get_slot('nome_tarefa')
 
         if nome_tarefa and nome_usuario:
-            dispatcher.utter_message(f"Tarefa: {nome_tarefa}\nUsuário: {nome_usuario}")
+            dispatcher.utter_message(f"Tarefa: {nome_tarefa}\nUsuário: {nome_usuario}\nTarefa Cadastrada com Sucesso")
+            # Example of using the class
+            connection = MongoDBConnection(settings.DB_NAME)
+            connection.connect()
+
+            # Perform database operations here, for example:
+            collection = connection.db[settings.DB_COLLECTION]
+            document = {
+                "nome_usuario": nome_usuario,
+                "nome_tarefa": nome_tarefa
+            }
+            collection.insert_one(document)
+            
+            connection.disconnect()
         else:
             dispatcher.utter_message("Deu ruim...")
 
         return []
 
-def autenticar_usuario(nome, senha):
-    
-    return False
+print("db")
